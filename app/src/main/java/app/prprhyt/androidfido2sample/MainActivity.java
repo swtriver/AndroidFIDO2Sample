f49009db4c58624243bf8a71817f6544718fbb56
prprhyt <prprhyt@gmail.com>
2018-07-15 00:47:01 +0900

    add dependencies for fido2.0

--- app/src/main/java/app/prprhyt/androidfido2sample/MainActivity.java
+++ app/src/main/java/app/prprhyt/androidfido2sample/MainActivity.java
@@ -2,6 +2,8 @@ package app.prprhyt.androidfido2sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.fido.fido2.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
